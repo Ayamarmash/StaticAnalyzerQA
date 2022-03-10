0d46a01a78d267baf32c0c591f6d5d05ae19fe10
lmaaljohare <68349552+lmaaljohare@users.noreply.github.com>
2022-03-10 20:04:02 +0200

    add Attributes test

--- StaticAnalyzerQA/StaticAnalyzerQA.py
+++ StaticAnalyzerQA/StaticAnalyzerQA.py
@@ -21,3 +21,75 @@ for line in Lines:
         for x in range (10):
             if str(x) in line:
                 print("Line : ",line , "Contains Magic Number")
+                
+               
+#####################
+## attributes test
+def GetTypeFromFunction(line, variable):
+    line = line.replace("(","")
+    line = line.replace(")","")
+    line = line.replace(","," ")
+    line = line.split()
+    return line[(line.index(variable))-1]
+    
+
+def GetTypeFromDefenion(lines, variable):
+    DefLine = ""
+    for line in lines:
+        if line.__contains__(variable+"=") or line.__contains__(variable+" ="):
+            DefLine = line
+            break
+    # print(variable+"=")        
+    DefLine = DefLine.replace(","," ")
+    DefLine = DefLine.split()
+    # print (DefLine)
+    return DefLine[DefLine.index(variable)-1]
+
+def GetVarAndTypes (FunLine):
+    variables = []
+    types = []
+    FunLine = FunLine[FunLine.find("("):FunLine.find(")")]
+    FunLine = FunLine.replace("( ","")
+    FunLine = FunLine.replace(" )","")
+    FunLine = FunLine.replace("(","")
+    FunLine = FunLine.replace(")","")
+    FunLine = FunLine.replace(", ",",")
+    FunLine = FunLine.replace(" ,",",")
+    FunLine = FunLine.split(",")
+    # print(FunLine)
+    switch = 0
+    for section in FunLine:
+        section = section.split(" ")
+        for word in section:
+            if switch == 0:
+                variables.append(word)
+                switch = 1
+            else:
+                types.append(word)
+                switch = 0
+    return variables , types
+
+
+def Attributes (lines):
+    defType=[]
+    dataTypes = ['int', 'void', 'bool', 'double', 'float', 'long']
+    variables = []
+    funTypes = []
+    for line in lines:
+        for type in dataTypes:
+            if type in line and '{' in line and '(' in line and ';' not in line:
+                funTypes, variables = GetVarAndTypes(line)
+    
+    
+    for variable in variables:
+        defType.append(GetTypeFromDefenion(lines, variable))
+    # print(variables) 
+    # print (funTypes)
+    # print (defType)
+    for i in range(len(funTypes)):
+        if defType[i] != funTypes[i]:
+            print('{',variables[i],'} data type of ', defType[i], ' has called by wrong datatype ', funTypes[i])
+            
+
+Attributes(readFile())
+                
